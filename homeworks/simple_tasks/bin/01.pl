dacf92e985415daf548ce6d0e02df41525ccb0f7
PavelBMSTU <pasch.sockolow2012@yandex.ru>
2017-03-03 12:20:15 +0300

    Add files via upload

--- homeworks/simple_tasks/bin/01.pl
+++ homeworks/simple_tasks/bin/01.pl
@@ -31,16 +31,21 @@ sub run {
 
     my $x1 = undef;
     my $x2 = undef;
-    if ($a_value == 0) {
-        while ($a_value == 0) {
-            $a_value = @_;
-        }
-    }
     
-    if (($b_value**2 - 4*$a_value*$c_value) >= 0) {
-        $x1 = ((-1)*$b_value + ($b_value**2 - 4*$a_value*$c_value)**(1/2))/(2*$a_value);
-        $x2 = ((-1)*$b_value - ($b_value**2 - 4*$a_value*$c_value)**(1/2))/(2*$a_value);
+    my $D = $b_value**2 - (4*$a_value*$c_value);
+    
+    
+    
+    if ($D >= 0) {
+        if ($a_value == 0) {
+            $x1 = (-1)*$c_value/$b_value;
+            print "$x1\n";
+        }
+        if ($a_value != 0) {
+        $x1 = ((-1)*$b_value + ($D)**(1/2))/(2*$a_value);
+        $x2 = ((-1)*$b_value - ($D)**(1/2))/(2*$a_value);
         print "$x1, $x2\n";
+    }
     }
     else {
         print "No solution!\n";
